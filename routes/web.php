d1979b82bcf934e122a487df85bc84c7bbaf54e5
Clement Couturier <ccouturier@b2pweb.com>
2024-03-21 11:38:26 +0100

    (+] Nouvel onglet Calendrier dans la navbar

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EventController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,7 +17,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::resource('events', \App\Http\Controllers\EventController::class);
+    Route::resource('events', EventController::class);
     Route::get('refetch-events', '\App\Http\Controllers\EventController@refetchEvents')->name('refetch-events');
     Route::put('events/{id}/resize', '\App\Http\Controllers\EventController@resizeEvent')->name('resize-event');
 });
