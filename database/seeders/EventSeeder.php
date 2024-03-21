afa96d22499280b062a65f5a9c40b9a61064adc4
Clement Couturier <ccouturier@b2pweb.com>
2024-03-21 16:23:57 +0100

    [+] Correction des paramètres du seeder

--- database/seeders/EventSeeder.php
+++ database/seeders/EventSeeder.php
@@ -16,36 +16,36 @@ class EventSeeder extends Seeder
         Event::factory(8)
             ->sequence(
                 [
-                    'starts_at' => now()->setTime(9, 0),
-                    'ends_at' => now()->setTime(10, 0),
+                    'start' => now()->setTime(9, 0),
+                    'end' => now()->setTime(10, 0),
                 ],
                 [
-                    'starts_at' => now()->setTime(11, 0),
-                    'ends_at' => now()->setTime(12, 0),
+                    'start' => now()->setTime(11, 0),
+                    'end' => now()->setTime(12, 0),
                 ],
                 [
-                    'starts_at' => now()->setTime(14, 0),
-                    'ends_at' => now()->setTime(16, 0),
+                    'start' => now()->setTime(14, 0),
+                    'end' => now()->setTime(16, 0),
                 ],
                 [
-                    'starts_at' => now()->setTime(11, 30),
-                    'ends_at' => now()->setTime(12, 30),
+                    'start' => now()->setTime(11, 30),
+                    'end' => now()->setTime(12, 30),
                 ],
                 [
-                    'starts_at' => now()->setTime(15, 10),
-                    'ends_at' => now()->setTime(16, 20),
+                    'start' => now()->setTime(15, 10),
+                    'end' => now()->setTime(16, 20),
                 ],
                 [
-                    'starts_at' => now()->setTime(8, 0),
-                    'ends_at' => now()->setTime(12, 0),
+                    'start' => now()->setTime(8, 0),
+                    'end' => now()->setTime(12, 0),
                 ],
                 [
-                    'starts_at' => now()->setTime(15, 0),
-                    'ends_at' => now()->setTime(19, 0),
+                    'start' => now()->setTime(15, 0),
+                    'end' => now()->setTime(19, 0),
                 ],
                 [
-                    'starts_at' => now()->setTime(20, 40),
-                    'ends_at' => now()->setTime(22, 10),
+                    'start' => now()->setTime(20, 40),
+                    'end' => now()->setTime(22, 10),
                 ],
             )
             ->create();
