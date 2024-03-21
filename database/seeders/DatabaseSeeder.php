68163e3a6168f402e0db5da5d6a2a507bbcf2190
Clement Couturier <ccouturier@b2pweb.com>
2024-03-21 16:24:20 +0100

    [~] Changement de l'ordre d'exécution des seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Enum\RoleEnum;
+use App\Models\AssoGroupUser;
+use App\Models\Group;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
@@ -15,41 +17,42 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(EventSeeder::class);
-        $this->call(RoleSeeder::class);
-
+        
         $respRole = Role::firstWhere('name', RoleEnum::RESP_GROUP->value);
 
-        \App\Models\User::factory(10)->create()
+        User::factory(10)->create()
             ->each(
                 fn (User $user) => $user->assignRole($respRole)
             );
-        //        \App\Models\Group::factory(10)->create();
-
-        $userRole = Role::firstWhere('name', RoleEnum::USER->value);
-
-        \App\Models\User::factory(10)->create()
+            
+            $userRole = Role::firstWhere('name', RoleEnum::USER->value);
+            
+            User::factory(10)->create()
             ->each(
                 fn (User $user) => $user->assignRole($userRole)
             );
-
-        \App\Models\User::factory()->create([
-            'name' => 'Admin',
+            
+            User::factory()->create([
+                'name' => 'Admin',
             'email' => 'admin@test.com',
             'password' => 'password',
-        ])
+            ])
             ->assignRole(Role::firstWhere('name', RoleEnum::ADMIN->value));
-
-        $groupNames = ['GROUP1', 'GROUP2', 'GROUP3'];
+            
+            $groupNames = ['GROUP1', 'GROUP2', 'GROUP3'];
         foreach ($groupNames as $groupName) {
-            \App\Models\Group::factory(1)->create([
-                'name' => $groupName,
-                'description' => fake()->text,
-            ],
+            Group::factory(1)->create(
+                [
+                    'name' => $groupName,
+                    'description' => fake()->text,
+                ],
             );
         }
-
-        \App\Models\AssoGroupUser::factory(20)
-            ->create();
+        
+        AssoGroupUser::factory(20)
+        ->create();
+        
+        $this->call(EventSeeder::class);
+        $this->call(RoleSeeder::class);
     }
 }
