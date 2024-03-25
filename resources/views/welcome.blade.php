6f0cc6876ee57e0a151d8a220e5c57b5aa070167
Clement Couturier <ccouturier@b2pweb.com>
2024-03-25 11:45:03 +0100

    [-] Correction de conflits CSS

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -138,6 +138,7 @@
         [type=submit],
         button {
             -webkit-appearance: button;
+            appearance: button;
             background-color: transparent;
             background-image: none
         }
@@ -161,6 +162,7 @@
 
         [type=search] {
             -webkit-appearance: textfield;
+            appearance: textfield;
             outline-offset: -2px
         }
 
@@ -242,7 +244,6 @@
         svg,
         video {
             display: block;
-            vertical-align: middle
         }
 
         img,
